6c12032051449330fffbc7bbd438d1406381a677
Simone <Studente@LAPTOP-40NF5P1I>
2021-04-19 12:25:15 +0200

    Aggiornamento in classe

---
+++ src/esercizioscuola/EsercizioScuola.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizioscuola;
+
+/**
+ *
+ * @author Studente
+ */
+public class EsercizioScuola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ciao sono qui in mezzo a voi");
+    }
+    
+}
